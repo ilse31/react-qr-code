3484bc4773a0340f6e961a01b3a9780d6408e04b
Ilham Prasetya <ilham.prasetya@nawadata.id>
2023-05-28 00:24:05 +0700

    init apps

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+type Props = {};
+import "./App.css";
+
+const App = (props: Props) => {
+  return (
+    <div className='text-lg'>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugit, nemo modi
+      vel illo possimus maxime nulla ipsum neque quisquam! Alias inventore
+      excepturi qui amet quaerat maiores unde modi distinctio non.
+    </div>
+  );
+};
+
+export default App;
