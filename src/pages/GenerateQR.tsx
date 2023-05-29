ba747554bea2bb98e1160aaa21f022f84f22d7e4
ilse31 <ilhamprazsetyo33@gmail.com>
2023-05-29 13:46:47 +0700

    add download qrcode

--- src/pages/GenerateQR.tsx
+++ src/pages/GenerateQR.tsx
@@ -5,6 +5,28 @@ type Props = {};
 
 const GenerateQR = (props: Props) => {
   const [value, setValue] = React.useState("");
+
+  const onImageDownload = () => {
+    const svg = document.getElementById("qrcode");
+    const svgData = new XMLSerializer().serializeToString(
+      svg as unknown as Node
+    );
+    const canvas = document.createElement("canvas");
+    const ctx = canvas.getContext("2d");
+    const img = new Image();
+    img.onload = () => {
+      canvas.width = img.width;
+      canvas.height = img.height;
+      ctx?.drawImage(img, 0, 0);
+      const pngFile = canvas.toDataURL("image/png");
+      const downloadLink = document.createElement("a");
+      downloadLink.download = "QRCode";
+      downloadLink.href = `${pngFile}`;
+      downloadLink.click();
+    };
+    img.src = `data:image/svg+xml;base64,${btoa(svgData)}`;
+  };
+
   return (
     <div className='w-full h-full flex flex-col justify-center items-center bg-gray-100'>
       <div className='h-screen items-center flex gap-5 flex-col w-1/2'>
@@ -12,10 +34,21 @@ const GenerateQR = (props: Props) => {
         <input
           className='bg-white text-black border border-gray-400 rounded py-2 px-4 mt-5 w-full focus:outline-none focus:border-blue-500'
           type='text'
+          placeholder='Enter text to generate QR Code'
           value={value}
           onChange={(e) => setValue(e.target.value)}
         />
-        {value && <QRCode value={value} />}
+        {value && (
+          <div className='flex flex-col items-center'>
+            <QRCode id='qrcode' value={value} size={200} />
+            <button
+              className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-5'
+              onClick={() => onImageDownload()}
+            >
+              Download QR Code
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
